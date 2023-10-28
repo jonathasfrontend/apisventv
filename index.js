0391f9a50fe1ecc46cc9e4d8d7c53d1f5a5ba3d3
Jonathas <jonathass5678@gmail.com>
2023-10-28 18:25:23 -0300

    apisventv

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
@@ -12,14 +11,12 @@ const Esportes = require('./models/esports')
 const port = 3000;
 const app = express();
 
-// const corsOptions = {
-//   origin: 'http://seufront-end.com', // Substitua pelo domínio do seu front-end
-//   optionsSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: 'https://sventv.vercel.app/',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-// app.use(cors(corsOptions));
-
-app.use(cors());
 require('dotenv').config();
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
@@ -90,8 +87,8 @@ app.get('/content/v1/esportes', async (req, res) => {
   }
 })
 
-// mongoose.connect('mongodb+srv://root:dFrPbwloK4qEAnKy@cluster0.xvdlp.mongodb.net/tvchannels?retryWrites=true&w=majority')
-mongoose.connect('mongodb://uafricriq2tv3mzaadgq:bFM5qSXyjy4cjLwbJaq@bdyu84sluwtmytaphic6-mongodb.services.clever-cloud.com:2023/bdyu84sluwtmytaphic6')
+mongoose.connect('mongodb+srv://root:dFrPbwloK4qEAnKy@cluster0.xvdlp.mongodb.net/tvchannels?retryWrites=true&w=majority')
+// mongoose.connect('mongodb://uafricriq2tv3mzaadgq:bFM5qSXyjy4cjLwbJaq@bdyu84sluwtmytaphic6-mongodb.services.clever-cloud.com:2023/bdyu84sluwtmytaphic6')
 .then(()=>{console.log("Banco de dados conectado!")})
 .catch(()=>{console.log("Falha ao conectar com o banco!")});
 
